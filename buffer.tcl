14d2e226b80f7f5550c20d535f6c8ad12806e44f
Anderson Domingues <ti.andersondomingues@gmail.com>
2019-11-18 15:27:20 -0300

    added gitinore to exclude jasper tmp folder, finished tcl for buffers

--- buffer.tcl
+++ buffer.tcl
@@ -1,10 +1,12 @@
 clear -all
+
+# hdl files must be declared respecting dependence order
 check_sec -setup \
 	-spec_top Hermes_buffer \
-	-spec_analyze_opts { -vhdl ../buffer-double-edge/Hermes_buffer.vhd } \
+	-spec_analyze_opts { -vhdl rtl/HeMPS_defaults.vhd rtl/Hermes_buffer_spec.vhd } \
 	-spec_elaborate_opts { -vhdl } \
 	-imp_top Hermes_buffer \
-	-imp_analyze_opts { -vhdl ../buffer-posedge-only/Hermes_buffer.vhd } \
+	-imp_analyze_opts { -vhdl rtl/HeMPS_defaults.vhd rtl/Hermes_buffer_imp.vhd } \
 	-imp_elaborate_opts { -vhdl } \
 
 #clock and reset setup
@@ -14,17 +16,23 @@ reset -expression {reset = '1'}
 #instruct sec to automatically map unitialized registers
 check_sec -auto_map_reset_x_values on
 
-#map relevant internal signals for fev (ALL SIGNALS ARE THE SAME)
-#check_sec -map -spec {{EA(2 DOWNTO 0)}} -imp {{sanduiche_ft_imp.EA(2 DOWNTO 0)}}
-#check_sec -map -spec {{grana(4 DOWNTO 0)}} -imp {{sanduiche_ft_imp.grana(4 DOWNTO 0)}}
+#map relevant internal signals for fev (commented out due 
+#to the tool does it automatically)
+#check_sec -map -spec {{Hermes_buffer_spec.credit_o}} -imp {{Hermes_buffer_imp.credit_o}}
+#check_sec -map -spec {{Hermes_buffer_spec.h}} -imp {{Hermes_buffer_imp.h}}
+#check_sec -map -spec {{Hermes_buffer_spec.data}} -imp {{Hermes_buffer_imp.data}}
+#check_sec -map -spec {{Hermes_buffer_spec.data_av}} -imp {{Hermes_buffer_imp.data_av}}
 
 #assumptions?
+
+#update sec
+check_sec -generate_verification
 
 #checking for mapping issues
 check_sec -interface
 
 #generate verification environment
-check_sec -gen
+#check_sec -gen
 
 check_sec -prove
 check_sec -signoff
